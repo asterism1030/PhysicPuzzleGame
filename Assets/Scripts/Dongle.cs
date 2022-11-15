c02739d649c58d17eceb90e1800c5785adc6e0e7
asterism1030 <rlaguswl1030@naver.com>
2022-11-15 22:24:36 +0900

    기본 구성
    UI 구성
    - 배경, 좌우 벽 및 하단의 바닥, 구분선
    - Touch Pad 구성
    둥글이의 드래그 드랍 기능 구현

---
+++ Assets/Scripts/Dongle.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dongle : MonoBehaviour
+{
+    public bool isDrag;
+    Rigidbody2D rigid;
+
+    // MonoBehaviour
+    void Awake()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        if(isDrag) {
+            Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            // x축 경계 설정 양쪽 Wall 의 두께 +- 둥글 반지름
+            float leftBorder = -4.0f + transform.localScale.x / 2;
+            float rightBorder = 4.0f - transform.localScale.y / 2;
+
+            if(mousePos.x < leftBorder) {
+                mousePos.x = leftBorder;
+            }
+            else if(mousePos.x > rightBorder) {
+                mousePos.x = rightBorder;
+            }
+
+            mousePos.y = 8;
+            mousePos.z = 0;
+            transform.position = Vector3.Lerp(transform.position, mousePos, 0.01f);
+        }
+
+                
+
+    }
+
+    // method
+    public void Drag()
+    {
+        isDrag = true;
+    }
+
+    public void Drop()
+    {
+        isDrag = false;
+        rigid.simulated = true;
+    }
+}

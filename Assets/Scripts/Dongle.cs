4d018f4dbfb682461d76600e9e7d4befa179a260
asterism1030 <rlaguswl1030@naver.com>
2022-11-16 19:49:06 +0900

    동글 생성
    드래그 드랍 후 일정 시간 지나면 새 동글 오브젝 생성
    동글의 애니메이터, 애니메이션 설정

--- Assets/Scripts/Dongle.cs
+++ Assets/Scripts/Dongle.cs
@@ -4,13 +4,21 @@ using UnityEngine;
 
 public class Dongle : MonoBehaviour
 {
+    public int level;
     public bool isDrag;
     Rigidbody2D rigid;
+    Animator anim;
 
     // MonoBehaviour
     void Awake()
     {
         rigid = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+    }
+
+    void OnEnable()
+    {
+        anim.SetInteger("Level", level);
     }
 
     void Update()
